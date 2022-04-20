fb7f7f2baf02cb675caec8677e40248f7be7fe5c
cxz <2405819294@qq.com>
2022-04-20 09:32:54 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>hi</div>
+  <momo></momo>
+</template>
+<script>
+import momo from './components/momo.vue';
+
+export default {
+  name: 'App',
+  components: {
+    momo,
+  }
+}
+</script>
