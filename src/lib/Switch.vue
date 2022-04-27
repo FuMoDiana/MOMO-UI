9a9d62304935c045a377bb91fb161076f02e3a75
cxz <2405819294@qq.com>
2022-04-27 09:24:45 +0800

    switch切换功能

---
+++ src/lib/Switch.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <Button  @click="toggle" :class="{checked}">
+      <span></span>  
+    </Button>
+  </div>
+</template>
+<script lang="ts">
+import { ref } from 'vue'
+export default {
+  setup() {
+    const checked = ref(false);
+    const toggle = ()=>{
+      checked.value = !checked.value;
+    }
+    return {checked,toggle}
+  }
+}
+</script>'
+
+<style lang="scss" scoped>
+  $h:22px;
+  $h2:18px;
+  button{
+    height: $h;
+    width: $h*2;
+    border: none;
+    background: gray;
+    border-radius: $h/2;
+    position: relative;
+  }
+  span{
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2/2;
+  }
+
+  button.checked {
+    background: blue;
+  }
+  button.checked > span {
+    left:24px
+  }
+</style>
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -6,16 +6,16 @@
               <h2>组件列表</h2>
               <ol>
                 <li>
-                    <router-link to="/doc/switch">Switch 组件</router-link>
+                    <router-link to="/doc/switch">Switch 开关</router-link>
                 </li>
                 <li>
-                    <router-link to="/doc/button">Button 组件</router-link>
+                    <router-link to="/doc/button">Button 按钮</router-link>
                 </li>
                 <li>
-                    <router-link to="/doc/dialog">Dialog 组件</router-link>
+                    <router-link to="/doc/dialog">Dialog 弹出框</router-link>
                 </li>
                 <li>
-                    <router-link to="/doc/tabs">Tabs 组件</router-link>
+                    <router-link to="/doc/tabs">Tabs 标签</router-link>
                 </li>
               </ol>
             </aside>
