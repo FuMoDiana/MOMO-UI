0442ba7d3c91fb66e940c6dbce170f2c5952076a
cxz <2405819294@qq.com>
2022-04-28 10:13:23 +0800

    优化按钮样式

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,10 +1,8 @@
 <template>
-    <div>
         <button class="Momo-button"
-            :class="`theme-${theme}`">
+            :class="`Momo-theme-${theme}`">
             <slot />
         </button>
-    </div>
 </template>
 
 <script lang="ts">
@@ -17,3 +15,41 @@ export default {
     }
 }
 </script>
+
+<style lang="scss">
+$h:32px;
+$radius:4px;
+.Momo-button {
+  box-sizing: border-box;
+  height: $h;
+  display: inline-flex;
+  cursor: pointer;
+  padding:0 12px;
+  border: none;
+  justify-content:center;
+  align-items: center;
+  white-space: nowrap;
+  border-radius: 9px;
+  font: normal 16px/normal "Times New Roman", Times, serif;
+  color: rgba(255,255,255,0.9);
+  -o-text-overflow: clip;
+  text-overflow: clip;
+  background: rgba(26,153,204,0.9);
+  box-shadow: 4px 4px 4px -1px rgba(0,0,0,0.2);
+  & + &{
+      margin-left: 8px;
+  }
+  &:hover,
+  &:focus{
+      background-color: rgb(153,221,153);
+      color: gray;
+  }
+  &:focus{
+      outline: none;
+  }
+  &::-moz-focus-inner{
+      border: 0;
+  }
+}
+
+</style>
