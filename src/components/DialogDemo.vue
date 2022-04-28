bf624a9ec09957b8caef58435618a99ca05cc3b8
cxz <2405819294@qq.com>
2022-04-28 16:55:56 +0800

    Dialog基础组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,3 +1,12 @@
 <template>
   <div>Dialog 示例</div>
+  <h1>示例一</h1>
+  <Dialog />
 </template> 
+
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue'
+export default {
+  components:{Dialog},
+}
+</script>
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="dialog-overlay"></div>
+    <div class="dialog-wrapper">
+        <header></header>
+        <main>
+            <p>mainmainmain</p>
+            <p>mainmainmain</p>
+        </main>
+        <footer>
+            <Button>OK</Button>
+            <Button>Cancel</Button>
+        </footer>
+    </div>
+</template>
+
+<script lang="ts">
+import Button from './Button.vue'
+export default {
+    components:{Button}
+}
+</script>
+
+<style lang="scss">
+    footer{
+        > Button{
+            background: white;
+           
+        }
+    }
+</style>
