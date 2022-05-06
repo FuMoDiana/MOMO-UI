27500c9d4745d748f587ff5684f2af3689c73266
cxz <2405819294@qq.com>
2022-05-06 11:13:28 +0800

    样式

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,11 +1,17 @@
 <template>
     <div class="topnav">
-            <div class="logo">LOGO</div>
+            <div class="logo">
+              <svg class="icon" aria-hidden="true">
+                <use xlink:href="#icon-Momo"></use>
+                <router-link to="/"></router-link>
+              </svg>
+            </div>
             <ul class="menu">
-                <li>菜单1</li>
-                <li>菜单2</li>
+                <li>
+                  <router-link to="/doc">文档</router-link>
+                </li>
             </ul>
-            <span class="toggleAside" @click="toggleMenu">
+            <span  class="toggleAside" @click="toggleMenu">
               <svg t="1650937293210" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2037" width="30" height="30"><path d="M170.666667 213.333333h682.666666v85.333334H170.666667V213.333333z m0 512h682.666666v85.333334H170.666667v-85.333334z m0-256h682.666666v85.333334H170.666667v-85.333334z" fill="#444444" p-id="2038"></path></svg>
             </span>
         </div>
@@ -37,6 +43,10 @@ export default {
   > .logo {
     max-width: 6em;
     margin-right: auto;
+    > svg{
+      width: 32px;
+      height: 32px;
+    }
   }
   > .menu {
     display: flex;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,10 +6,35 @@
             <h2>一个UI框架</h2>
             <p class="actions">
                 <a href="https://github.com/FuMoDiana/MOMO-UI" target="_blank">Github</a>
-                <router-link to="/doc">Start</router-link>
+                <router-link to="/doc" style="color:rgb(79,192,141);">Start</router-link>
             </p>
         </div>
     </div>
+  <div class="features">
+    <ul>
+      <li>
+        <svg>
+          <use xlink:href="#icon-Vue"></use>
+        </svg>
+        <h3>基于 Vue 3</h3>
+        <p>使用了 Vue 3 Composition API</p>
+      </li>
+      <li>
+        <svg>
+          <use xlink:href="#icon-ts"></use>
+        </svg>
+        <h3>基于 TypeScript </h3>
+        <p>源代码采用 TypeScript 书写（非严格检查）</p>
+      </li>
+      <li>
+        <svg>
+          <use xlink:href="#icon-light"></use>
+        </svg>
+        <h3>代码易读</h3>
+        <p>组件的源代码简单易读</p>
+      </li>
+    </ul>
+  </div>
 </template>
 <script lang="ts">
 import Topnav from '../components/Topnav.vue'
@@ -30,19 +55,58 @@ export default {
   > .actions{
     padding: 8px 0;
     a {
+      border: 1px solid rgb(79,192,141);
       margin:0 8px;
       background: #fff;
       display: inline-block;
-      $h: 28px;
-      height: $h;
-      line-height: $h;
-      border-radius: $h/2;
-      padding: 0 8px;
+      line-height: 28px;
+      border-radius: 20px;
+      padding: 8px 24px;
     }
   }
 }
 .topnavColors {
   background: rgb(189,236,201);
   background: radial-gradient(circle, rgba(189,236,201,1) 0%, rgba(117,212,134,0.9447129193474265) 100%);
+  clip-path: ellipse(85% 100% at 50% 0%);
+}
+
+.features {
+  margin: 64px auto;
+  width: 400px;
+  @media (min-width: 800px) {
+    width: 800px;
+  }
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+  >ul {
+    display: flex;
+    flex-wrap: wrap;
+    >li {
+      width: 400px;
+      margin: 16px 0;
+      display: grid;
+      justify-content: start;
+      align-content: space-between;
+      grid-template-areas:
+        "icon title"
+        "icon text";
+      grid-template-columns: 80px auto;
+      grid-template-rows: 1fr auto;
+      >svg {
+        grid-area: icon;
+        width: 64px;
+        height: 64px;
+      }
+      >h3 {
+        grid-area: title;
+        font-size: 28px;
+      }
+      >p {
+        grid-area: text
+      }
+    }
+  }
 }
 </style>
