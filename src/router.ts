8c298083a8f6b0d358798e0c75aaddeffc2b9258
cxz <2405819294@qq.com>
2022-05-09 16:27:39 +0800

    抽离出markdown组件，实现代码复用

--- src/router.ts
+++ src/router.ts
@@ -6,11 +6,11 @@ import ButtonDemo from "./components/ButtonDemo.vue";
 import DialogDemo from "./components/DialogDemo.vue";
 import TabsDemo from "./components/TabsDemo.vue";
 import DocDemo from "./components/DocDemo.vue";
-import Intro from './views/Intro.vue';
-import GetStarted from './views/GetStarted.vue'
-import Install from './views/Install.vue'
+import { h } from 'vue';
+import Markdown from './components/Markdown.vue';
 
 const history = createWebHashHistory();
+const md = filename => h(Markdown, { path: `../markdown/${filename}.md`, key: filename })
 export const router = createRouter({
   history: history,
   routes: [
@@ -19,9 +19,9 @@ export const router = createRouter({
       path: "/doc",
       component: Doc,
       children: [
-        { path: "intro", component: Intro },
-        { path: "get-started", component: GetStarted },
-        { path: "install", component: Install },
+        { path: "intro", component: md('intro') },
+        { path: "get-started", component: md('get-started') },
+        { path: "install", component: md('install') },
         { path: "switch", component: SwitchDemo },
         { path: "switch", component: SwitchDemo },
         { path: "button", component: ButtonDemo },
